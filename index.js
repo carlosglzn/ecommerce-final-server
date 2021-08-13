a2f20549cb5990401848352ab2bb35daae7a43d2
Carlos Gonzalez <carlosgonzalezn19@gmail.com>
2021-08-13 11:15:46 -0500

    server, get and post route test

---
+++ index.js
@@ -0,0 +1,37 @@
+// 1. IMPORTS
+
+const express = require('express')
+const app = express()
+const cors = require('cors')
+
+
+
+// 2. MIDDLEWARES
+
+require('dotenv').config()
+
+app.use(cors())
+app.use(express.json({extended: true}))
+
+// 3. ROUTES
+
+app.get('/', (req, res) => {
+
+    res.status(200).json({
+        message: 'Hello from server'
+    })
+
+})
+
+app.post('/data', (req, res) => {
+
+    res.status(200).json({
+        message: req.body
+    })
+})
+
+// 4. SERVER
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`)
+})
